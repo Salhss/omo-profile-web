9c3013ac7d597bc8d0c617c8b3b6243c637b23f3
unknown <salwa21salsabila@gmail.com>
2023-09-01 07:10:38 +0800

    fix: fix layout store

--- src/components/Store.jsx
+++ src/components/Store.jsx
@@ -5,12 +5,12 @@ export default function Store() {
         <h1 className="font-bold pt-40 text-[#8BCCD3] xs:text-2xl md:text-4xl">
           Where To Buy <span className="text-[#E4BA61] xs:text-3xl md:text-5xl">?</span>
         </h1>
-        <div className="flex justify-center items-center xs:mt-10 md:mt-20 md:gap-10 lg:gap-20">
+        <div className="flex justify-center items-center xs:flex-col xs:gap-5 xs:mt-10 md:flex-row md:mt-20 md:gap-10 lg:gap-20">
           <a href="https://shopee.co.id/omohealthysnack" target="_blank">
             <img
               src="https://logodownload.org/wp-content/uploads/2021/03/shopee-logo-0.png"
               alt=""
-              className="w-[300px]"
+              className="xs:w-[180px] md:w-[300px]"
               data-aos="fade-up"
             />
           </a>
@@ -18,7 +18,7 @@ export default function Store() {
             <img
               src="https://seeklogo.com/images/T/tokopedia-logo-40654CCDD6-seeklogo.com.png"
               alt=""
-              className="w-[250px]"
+              className="xs:w-[150px] xs:pb-20 md:pb-0 md:w-[250px]"
               data-aos="fade-up"
             />
           </a>
